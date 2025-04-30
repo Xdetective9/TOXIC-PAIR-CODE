8bfbb8d37151fc11481db0ad91cf618cf9af7604
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-04-30 22:53:33 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

--- netlify/functions/mbuvi.js
+++
@@ -1,9 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../../mbuvi');
-
-
-const handler = serverless(app);
-
-exports.handler = async (event, context) => {
-  return await handler(event, context);
-};
